3e3975ed30a8cf7e55d636e8af23480efe3c0819
mazin2014tt-netizen <mazin2014tt@gmail.com>
2026-01-10 10:46:52 +0300

    Update ajhsjsgd.lua

--- ajhsjsgd.lua
+++ ajhsjsgd.lua
@@ -1,1 +1,34 @@
+--====================================================
+-- Mangekyou Hub | Brookhaven RP
+-- Arabic GUI Script
+-- Delta Executor Compatible
+-- By Mazen
+--====================================================
+-- Keywords:
+-- Brookhaven Script
+-- Brookhaven RP
+-- Roblox Brookhaven GUI
+-- Arabic Script
+-- Delta Executor
+--====================================================
 
+-- Services
+local Players = game:GetService("Players")
+local UIS = game:GetService("UserInputService")
+local player = Players.LocalPlayer
+
+--========================
+-- CONFIG
+--========================
+local VALID_KEY = "مازن"
+local TOGGLE_KEY = Enum.KeyCode.RightShift
+
+--========================
+-- KEY SYSTEM GUI
+--========================
+local keyGui = Instance.new("ScreenGui")
+keyGui.Name = "MangekyouKeySystem"
+keyGui.ResetOnSpawn = false
+keyGui.Parent = player:WaitForChild("PlayerGui")
+
+local keyFrame
